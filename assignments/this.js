e3f2da1c1ca21d6e71e27a2ec2ab69bc9a94ec2c
Jason Cruz <badmonkey.dev@gmail.com>
2019-10-10 19:40:43 -0500

    met MVP

--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,60 @@
-/* The for principles of "this";
-* in your own words. explain the four principle for the "this" keyword below.
-*
-* 1. 
-* 2. 
-* 3. 
-* 4. 
-*
-* write out a code example of each explanation above
-*/
+// * 1. WINDOW / GLOBAL OBJECT BINDING
+// * When in the global scope, the value of “this” will be the window/console Object;
+// * window binding is like the forest of trees, not sure which one we are pointing at, so we point that the forrest
 
-// Principle 1
+function hello(name){
+   console.log('Hello ' + name);
+   console.log(this)
+}
 
-// code example for Window Binding
+hello('Jason');
 
-// Principle 2
+// * 2. IMPLICIT BINDING - (Automatic) with Objects and Methods
+// * Whenever a function is called by a preceding dot, the object before that dot is this.
 
-// code example for Implicit Binding
+const student = {
+   id: 7823,
+   name: 'Jason',
+   greeting: 'Hello ',
+   class: function(data){
+      console.log(this.greeting + data);
+      console.log(this);
+   }
+}
 
-// Principle 3
+student.class('Jason')
 
-// code example for New Binding
+// * 3. NEW BINDING
+// * Whenever a constructor function is used, this refers to the specific instance of the object that is created and returned by the constructor function.
 
-// Principle 4
+function Welcome(greeter) {
+   this.greeting = "Bueno dias!";
+   this.greeter = greeter;
+   this.speak = function() {
+      console.log( `${this.greeting} ${this.greeter}`);
+   }
+};
 
-// code example for Explicit Binding
+let greetJason = new Welcome('Jason');
+let greetSylvia = new Welcome('Sylvia');
+
+const spanishGreeter = new Welcome("Jason");
+
+spanishGreeter.speak();
+greetJason.speak();
+greetSylvia.speak();
+
+// * Principle 4
+
+// * 4. EXPLICIT BINDING
+// * Explicit Bindings are for functions, when JavaScript calls, applies, or binds method is used, this is explicitly defined. You can force a function call to use a particular object for this binding, without putting a property function reference on the object, so we explicitly say to a function what object it should use for this.
+
+function sayHello(){
+   console.log(this.name);
+}
+
+let person ={ 
+   name: 'Ruben',
+}
+
+sayHello.call(person);
